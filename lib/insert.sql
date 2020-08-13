74f6fb142a68cb07182a931bab34df4c36399e09
russellsutt <russell.sutter@gmail.com>
2020-08-12 20:51:30 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears VALUES
+(1, 'Mr. Chocolate', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES
+(2, 'Rowdy', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES
+(3, 'Tabitha', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES
+(4, 'Sergeant', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES
+(5, 'Melissa', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES
+(6, 'Grinch', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES
+(8, 'Wendy', 8, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES
+(9, NULL, 8, 'M', 'brown', 'calm', 1)
+;
